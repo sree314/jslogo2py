a969b33fd62717771959b6b4bad84d1d3fe912a5
sree <sree@buckyball>
2022-07-24 15:59:19 -0400

    Initial drafts of most of the commands

--- jslogort.py
+++ jslogort.py
@@ -18,25 +18,13 @@ class JSTurtle:
     cury = 0
     heading = 0
 
-    def position(self, xy):
-        pass
-
     def home(self):
         turtle.goto(0, 0)
         (self.curx, self.cury), self.heading = turtle.position(), turtle.heading()
 
-    def turtlemode(self, mode):
-        pass
-
-    def pendown(self, mode):
-        if mode:
-            turtle.pendown()
-        else:
-            turtle.penup()
-
     def move(self, distance):
         if distance < 0:
-            turtle.backward(distance)
+            turtle.backward(-distance)
         else:
             turtle.forward(distance)
 
@@ -50,3 +38,89 @@ class JSTurtle:
     def wait(self, time):
         # argument time is 1/60 of seconds
         time.sleep(time * 1.0/60.0)
+
+    def setvar(self, varname, value):
+        if varname == "led1":
+            turtle.leftLED = value != 0
+        elif varname == "led2":
+            turtle.rightLED = value != 0
+        elif varname == "ir1":
+            turtle.leftDetector = value != 0
+        elif varname == "ir2":
+            turtle.rightDetector = value != 0
+        elif varname == "emitter":
+            turtle.emitter = value != 0
+        else:
+            pass
+
+    def not_supported(self):
+        pass
+
+    def not_implemented(self):
+        pass
+
+    def clearscreen(self):
+        self.home()
+
+    def clear(self):
+        self.not_supported()
+
+    def drawtext(self, text):
+        self.not_implemented()
+
+    def fillpath(self, fillcolor):
+        self.not_supported()
+
+    def fill(self):
+        self.not_supported()
+
+    def arc(self, angle, radius):
+        self.not_implemented()
+
+    def getstate(self):
+        self.not_supported()
+
+    def setstate(self, state):
+        self.not_supported()
+
+    def turtlemode(self, mode):
+        self.not_supported()
+
+    def pendown(self, mode):
+        if mode:
+            turtle.pendown()
+        else:
+            turtle.penup()
+
+    def color(self, color):
+        self.not_implemented()
+
+    def bgcolor(self, color):
+        self.not_supported()
+
+    def penwidth(self, width):
+        self.not_supported()
+
+    def fontsize(self, size):
+        self.not_implemented()
+
+    def fontname(self, name):
+        self.not_implemented()
+
+    def position(self, xy):
+        turtle.goto(xy[0], xy[1])
+        (self.curx, self.cury), self.heading = turtle.position(), turtle.heading()
+
+    def heading(self, angle):
+        turtle.left(angle)
+
+    def visible(self, visible):
+        if not visible:
+            # don't know how to make things invisible
+            self.not_supported()
+
+    def scrunch(self, sc):
+        self.not_implemented()
+
+
+
